767cbae26260c8b095c4ade35d97ff689732eee0
Ekaterina_Ivanchenko <ESIvanchenko@outlook.com>
2021-05-11 15:24:18 -0700

    Added link to GitHub

--- src/App.js
+++ src/App.js
@@ -10,7 +10,14 @@ export default function App() {
         </header>
         <Dictionary />
         <footer className="footer">
-          <small>Coded by Ekaterina Ivanchenko</small>
+          <small>Coded by Ekaterina Ivanchenko and it is </small>
+          <a
+            href="https://github.com/Ivanceka/dictionary-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <small>open-source</small>
+          </a>
         </footer>
       </div>
     </div>
