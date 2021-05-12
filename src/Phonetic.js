25c9e28d3c4f1a46889977aaa6838369d5f99635
Ekaterina_Ivanchenko <ESIvanchenko@outlook.com>
2021-05-12 10:42:23 -0700

    Added volume icon

---
+++ src/Phonetic.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { VolumeNotice } from "@icon-park/react";
+
+export default function Phonetic(props) {
+  return (
+    <div className="Phonetic">
+      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
+        <VolumeNotice theme="outline" size="24" fill="#333" />
+      </a>{" "}
+      {props.phonetic.text}
+    </div>
+  );
+}
