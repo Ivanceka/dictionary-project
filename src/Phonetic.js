c832d2e3bb11d0df822d344877f3e6a587c4b19b
Ekaterina_Ivanchenko <ESIvanchenko@outlook.com>
2021-05-13 13:23:45 -0700

    Styled, added sound

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,12 +1,22 @@
 import React from "react";
 import { VolumeNotice } from "@icon-park/react";
+import useSound from "use-sound";
+import "./Phonetic.css";
 
 export default function Phonetic(props) {
+  let audio = props.phonetic.audio;
+  const [play] = useSound(audio, {
+    volume: 0.5,
+  });
+  function playAudio(event) {
+    event.preventDefault();
+    play();
+  }
   return (
     <div className="Phonetic">
-      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
-        <VolumeNotice theme="outline" size="24" fill="#3d84b8" />
-      </a>{" "}
+      <a href="/" rel="noreferrer" onClick={playAudio}>
+        <VolumeNotice theme="outline" size="24" />
+      </a>
       {props.phonetic.text}
     </div>
   );
